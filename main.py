ed1d6b870c557eee1fa7333470391cb0d3338bee
RIONAGANO19990620 <konsyoji@gmail.com>
2022-05-09 20:49:51 +0900

    refactor

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     v_noisy, u_noisy = DataFactory.get_teacher_data()
     t_array = DataFactory.t
     nn = PhysicsInformedNN(t_array, u_noisy, v_noisy)
-    nn.train(10000)
+    nn.train(1000)
     nn.compare_numerical_ans()
 
 
