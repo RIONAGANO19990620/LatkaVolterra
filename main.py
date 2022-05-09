450f9c5da98a31756c27f1ed511047255953fa57
RIONAGANO19990620 <konsyoji@gmail.com>
2022-05-09 21:13:31 +0900

    refactor

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from NeuralNet import PhysicsInformedNN
 def main():
     v_noisy, u_noisy = DataFactory.get_teacher_data()
     v, u = DataFactory.get_lotka_volterra()
+    v = (v - v.min())/(v.max() - v.min())
+    u = (u - u.min())/(u.max() - u.min())
     t_array = DataFactory.t
     nn = PhysicsInformedNN(t_array, u_noisy, v_noisy)
     nn.train(10000)
