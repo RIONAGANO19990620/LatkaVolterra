56f9e5ebdadd4bff8908b2a9f33ac67c8cd60bc0
Takasu Ryoma <99372283+ryoma-takasu@users.noreply.github.com>
2022-06-06 11:37:01 +0900

    change loss function

--- Model.py
+++ Model.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow import keras
+import numpy as np
 
 from NormalizedData import NormalizedDataList
 
@@ -11,10 +12,10 @@ class PPINs(keras.Model):
 
     def __init__(self, normalized_data_list: NormalizedDataList, *args, **kwargs):
         super(PPINs, self).__init__(*args, **kwargs)
-        self.a = tf.Variable([0], dtype=tf.float32)
-        self.b = tf.Variable([0], dtype=tf.float32)
-        self.c = tf.Variable([0], dtype=tf.float32)
-        self.d = tf.Variable([0], dtype=tf.float32)
+        self.a = tf.Variable([0.5], dtype=tf.float32)
+        self.b = tf.Variable([0.5], dtype=tf.float32)
+        self.c = tf.Variable([0.5], dtype=tf.float32)
+        self.d = tf.Variable([0.5], dtype=tf.float32)
         self.normalized_data_list = normalized_data_list
 
     def train_step(self, data):
@@ -22,17 +23,14 @@ class PPINs(keras.Model):
 
         with tf.GradientTape() as tape:
             y_pred = self(x, training=True)
-            loss = tf.reduce_mean(tf.reduce_mean(tf.square(y['u_data'] - y_pred[:, 0]))) + tf.reduce_mean(
-                tf.square(y['v_data'] - y_pred[:, 1])) + tf.reduce_mean(
-                tf.square(PPINs.pred(y_pred, x, self.a, self.b, self.c, self.d, self.normalized_data_list)))
+            loss = tf.reduce_mean(tf.reduce_mean(tf.square(y['u_data'] - y_pred[:, 0]))) + tf.reduce_mean(tf.square(y['v_data'] - y_pred[:, 1])) + PPINs.pred(y_pred, x, self.a, self.b, self.c, self.d, self.normalized_data_list)
         trainable_vars = self.trainable_variables
         gradients = tape.gradient(loss, trainable_vars)
         self.optimizer.apply_gradients(zip(gradients, trainable_vars))
         loss_tracker.update_state(loss)
         mae_metric.update_state(y['u_data'], y_pred[:, 0])
         mae_metric.update_state(y['v_data'], y_pred[:, 1])
-        return {"loss": loss_tracker.result(), "mae": mae_metric.result(), 'a': self.a, "b": self.b, 'c': self.c,
-                'd': self.d}
+        return {"loss": loss_tracker.result(), "mae": mae_metric.result(), 'a': self.a, "b": self.b, 'c': self.c,'d': self.d}
 
     @staticmethod
     def pred(y_pred, x, a, b, c, d, normalized_data_list: NormalizedDataList):
@@ -43,4 +41,6 @@ class PPINs(keras.Model):
         t_m = normalized_data_list.t_normalized.max - normalized_data_list.t_normalized.min
         u_t = tf.divide(u_t, t_m)
         v_t = tf.divide(v_t, t_m)
-        return v_t - c * u * v + d * v + u_t - a * u + b * u * v
+        loss1 = tf.reduce_mean(tf.square(v_t - c * u * v + d * v))
+        loss2 = tf.reduce_mean(tf.square(u_t - a * u + b * u * v))
+        return loss1 + loss2
